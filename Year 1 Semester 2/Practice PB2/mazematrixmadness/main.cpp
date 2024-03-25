25171a8353677db3696ba103e4ec0430a93e9ea3
KevinOctavius <145738604+KevinOctavius@users.noreply.github.com>
2024-03-25 21:28:14 +0700

    Added mazematrix folder

---
+++ Year 1 Semester 2/Practice PB2/mazematrixmadness/main.cpp
@@ -0,0 +1,115 @@
+#include <iostream>
+#include <algorithm>
+
+class Maze {
+    private:
+        char** env;
+        int rows;
+        int cols;
+    public: 
+        Maze();
+        ~Maze();
+        Maze(const Maze &);
+        Maze& operator=(Maze);
+
+        void getSize() const;
+        void readMazeFromTerminal();
+        void printMaze() const;
+};
+
+Maze::Maze() : rows(0), cols(0) {}
+
+Maze::~Maze() {
+    for (int i = 0; i < rows; i++) {
+        delete[] *(env + i);
+    }
+    
+    delete[] env;
+}
+
+Maze::Maze(const Maze &maze) : rows(maze.rows), cols(maze.cols) {
+    env = new char*[maze.rows];
+
+    for (int i = 0; i < maze.rows; i++) {
+        char *temp = new char[maze.cols];
+
+        for (int j = 0; j < maze.cols; j++) {
+            *(temp + j) = *(*(maze.env + i) + j);
+        }
+
+        *(env + i) = temp;
+    }
+}
+
+Maze& Maze::operator=(Maze other) {
+    std::swap(env, other.env);
+    std::swap(rows, other.rows);
+    std::swap(cols, other.cols);
+
+    return *this;
+}
+
+void Maze::getSize() const {
+    std::cout << "Rows: " << rows << std::endl << "Columns: " << cols << std::endl;
+}
+
+void Maze::readMazeFromTerminal() {
+    std::cout << "Enter Row Size: ";
+    std::cin >> rows;
+
+    std::cout << std::endl;
+
+    std::cout << "Enter Column Size: ";
+    std::cin >> cols;
+
+    std::cout << std::endl << std::endl;
+
+    env = new char*[rows];
+
+    for (int i = 0; i < rows; i++) {
+        char *temp = new char[cols];
+
+        for (int j = 0; j < cols; j++) {
+            std::cin >> *(temp + j);
+        }
+
+        *(env + i) = temp;
+    }
+}
+
+void Maze::printMaze() const {
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            std::cout << *((*(env + i)) + j) << " ";
+        }
+
+        std::cout << std::endl;
+    }
+}
+
+int main() {
+    
+    Maze maze = Maze();
+    maze.readMazeFromTerminal();
+
+    std::cout << std::endl;
+
+    std::cout << "maze.printMaze():" << std::endl;
+    maze.printMaze();
+
+    std::cout << std::endl;
+
+    Maze copyMaze = Maze(maze);
+
+    std::cout << "copyMaze.printMaze():" << std::endl;
+    copyMaze.printMaze();
+
+    std::cout << std::endl;
+
+    Maze copyMaze2 = maze;
+
+    std::cout << "copyMaze2.printMaze():" << std::endl;
+    copyMaze.printMaze();
+    
+    return 0;
+}
